e19fa7c786d8a3d2085ff0acfabe74f449ae6515
Andrei Bartashevich <andrei.bartashevich@krontech.ru>
2020-04-05 00:35:42 +0300

    Allow console.warn and console.error in production

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production'
+      ? ['error', { allow: ['warn', 'error'] }]
+      : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // Переопределения правил
     'space-before-function-paren': ['error', 'never'],
